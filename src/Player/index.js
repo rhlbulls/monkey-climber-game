711cd0e70e6aec598a80182257f89a456dae05aa
rhlbulls <rahulbulls12@gmail.com>
2024-12-22 17:08:04 +0530

    refactoring for player movement

--- src/Player/index.js
+++ src/Player/index.js
@@ -1,4 +1,3 @@
-
 export function createPlayer(scene, x, y, texture) {
     const player = scene.physics.add.sprite(x, y, texture);
 
@@ -10,78 +9,67 @@ export function createPlayer(scene, x, y, texture) {
     player.speed = 350;
     player.width = 200;
     player.height = 200;
-    player.rightMove = 0;
-    player.leftMove = 0;
-    player.upMove = 0
-    player.downMove = 0;
+    player.movement = {
+        rightMove: 0,
+        leftMove: 0,
+        upMove: 0,
+        downMove: 0
+    };
+    player.isChangingTexture = false;
+    player.isMovingLeft = false;
+    player.isMovingRight = false;
+
     scene.add.existing(player);
 
     return player;
 }
 
+function changeTextureSequentially(player, textures, interval) {
+    if (player.isChangingTexture) return;
+
+    player.isChangingTexture = true;
+    let index = 0;
+
+    const textureInterval = setInterval(() => {
+        player.setTexture(textures[index]);
+        index++;
+
+        if (index >= textures.length) {
+            clearInterval(textureInterval);
+            player.isChangingTexture = false;
+        }
+    }, interval);
+}
+
 export function handlePlayerInput(player, cursorKeys) {
+    const { left, right, up, down } = cursorKeys;
+
     if (!player.body.blocked.down) {
-        if (cursorKeys.left.isDown && player.isMovingLeft) {
+        if (left.isDown && player.isMovingLeft) {
             player.setTexture("monkey_left_jump_1");
-        } else if (cursorKeys.right.isDown) {
+        } else if (right.isDown) {
             player.setTexture("monkey_right_jump_1");
         }
-    }
-    else if (player.body.blocked.down) {
-        if (cursorKeys.up.isDown) {
-            player.setVelocityY(-player.speed*0.65);
-        } else if (cursorKeys.down.isDown) {
-            player.setVelocityY(player.speed*0.65);
-        } else if (cursorKeys.left.isDown) {
+    } else {
+        if (up.isDown) {
+            player.setVelocityY(-player.speed * 0.65);
+        } else if (down.isDown) {
+            player.setVelocityY(player.speed * 0.65);
+        } else if (left.isDown) {
             player.isMovingLeft = true;
             player.isMovingRight = false;
-            if (!player.isChangingTexture) {
-                player.isChangingTexture = true;
-                setTimeout(() => {
-                    player.setTexture("monkey_left_1");
-                }, 0);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_left_2");
-                }, 100);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_left_3");
-                }, 200);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_left_1");
-                    player.isChangingTexture = false;
-                }, 300);
-            }
-
             player.setVelocityX(-player.speed);
-        } else if (cursorKeys.right.isDown) {
+
+            changeTextureSequentially(player, ["monkey_left_1", "monkey_left_2", "monkey_left_3", "monkey_left_1"], 100);
+        } else if (right.isDown) {
             player.isMovingRight = true;
             player.isMovingLeft = false;
-            if (!player.isChangingTexture) {
-                player.isChangingTexture = true;
-
-                setTimeout(() => {
-                    player.setTexture("monkey_right_1");
-                }, 0);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_right_2");
-                }, 100);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_right_3");
-                }, 200);
-
-                setTimeout(() => {
-                    player.setTexture("monkey_right_1");
-                    player.isChangingTexture = false;
-                }, 300);
-            }
             player.setVelocityX(player.speed);
+
+            changeTextureSequentially(player, ["monkey_right_1", "monkey_right_2", "monkey_right_3", "monkey_right_1"], 100);
         } else {
             player.setVelocityX(0);
+
             if (player.isMovingLeft) {
                 player.setTexture("monkey_left_1");
             } else {
